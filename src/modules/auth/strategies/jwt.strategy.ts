7ffe356de3c938930805888b43a46d96b64d8eb9
git <laghomklaus@yahoo.fr>
2026-09-01 19:05:54 +0100

    Add JWT fallback secret

--- src/modules/auth/strategies/jwt.strategy.ts
+++ src/modules/auth/strategies/jwt.strategy.ts
@@ -16,7 +16,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: configService.get('JWT_ACCESS_SECRET'),
+      secretOrKey: configService.get('JWT_ACCESS_SECRET') || 'fallback_secret_change_me',
     });
   }
 
